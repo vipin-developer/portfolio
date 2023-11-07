f816a933db9e54bf0ff2cf0953378f7ee1b4619e
Vipin Kumar <vipinkumar@Salonis-MacBook-Air.local>
2023-11-08 02:03:28 +0530

    home page is done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./Home.css";
+import Lottie from "lottie-react";
+import dev from '../animations/dev.json'
 
 function Home(props) {
   return (
     <div className="home">
-      <div>
+      <div className="intro">
         <h4>Hi, My Name is</h4>
         <h1>Vipin Kumar</h1>
-        <h2>I build Web apps</h2>
+        <h2> I code to build websites, mobile apps,
+</h2>
+<h2> Rest APIs.
+</h2>
       </div>
       <div>
-        
+      <Lottie animationData={dev} className="dev-animation" loop={true} />
       </div>
     </div>
   );
