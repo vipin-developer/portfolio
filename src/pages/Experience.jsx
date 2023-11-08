0536f657b5116188de3a517fb802d2cc0c5deaf3
Vipin Kumar <vipinkumar@Salonis-MacBook-Air.local>
2023-11-08 23:53:50 +0530

    working on experience page

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
 
-function Experience(props) {
-    return (
-        <div className='experience'>
-            Experience
-        </div>
-    );
+import "./Experience.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+const WorkIcon = () => <></>;
+
+export default function Experience() {
+  return (
+    <div className="experience Experience-main">
+      <h2 className="skill-text exp">My Experience</h2>
+      
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "#9970f9", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="Aug 2023 - present"
+          iconStyle={{ background: "#9970f9", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h2 className="vertical-timeline-element-title">Senior Software Engineer</h2>
+          <h2 className="vertical-timeline-element-subtitle">Epam Systems, India</h2>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project
+            Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2010 - 2011"
+          iconStyle={{ background: "#9970f9", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Art Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            San Francisco, CA
+          </h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, SEO, Online
+            Marketing
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2008 - 2010"
+          iconStyle={{ background: "#9970f9", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Web Designer</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Los Angeles, CA
+          </h4>
+          <p>User Experience, Visual Design</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2006 - 2008"
+          iconStyle={{ background: "#9970f9", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Web Designer</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            San Francisco, CA
+          </h4>
+          <p>User Experience, Visual Design</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
-
-export default Experience;
